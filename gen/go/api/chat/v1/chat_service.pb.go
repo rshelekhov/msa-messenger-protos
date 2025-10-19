26906b0bea4db87e0d667c593182a2f4f2c31879
Roman Shelekhov <shelekhoff@gmail.com>
2025-10-19 22:37:15 +0400

    refactor: update AcceptFriendInviteRequest to use invite_id
    - Change field name from user_id to invite_id in AcceptFriendInviteRequest to better reflect its purpose.
    - Update validation logic and regex pattern to match the new field name.
    - Adjust related code in generated files to ensure consistency across the application.

--- gen/go/api/chat/v1/chat_service.pb.go
+++ gen/go/api/chat/v1/chat_service.pb.go
@@ -7,11 +7,10 @@
 package chat
 
 import (
-	reflect "reflect"
-
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
+	reflect "reflect"
 )
 
 const (
